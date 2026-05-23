74987bb6d8c9873f190b682730709f94805a9d8a
MSTR FF <wadamak2001@gmail.com>
2026-05-23 11:03:01 +0000

    fix: use npm run build in workflow

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,17 @@
-
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
- // output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+  output: 'export',
+    images: {
+        unoptimized: true,
+          },
+            typescript: {
+                ignoreBuildErrors: true,
+                  },
+                    eslint: {
+                        ignoreDuringBuilds: true,
+                          },
+                          };
 
-export default nextConfig;
+                          export default nextConfig;
+                          
