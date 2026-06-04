bc250792ff9daa46419877ce0b24acb39744c9a6
wadamak2001-hue <wadamak2001@gmail.com>
2026-06-04 13:44:11 +0200

    Update MainActivity.java

--- android/app/src/main/java/com/voidboost/optimizer/MainActivity.java
+++ android/app/src/main/java/com/voidboost/optimizer/MainActivity.java
@@ -1,5 +1,21 @@
 package com.voidboost.optimizer;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+    }
+}
+
